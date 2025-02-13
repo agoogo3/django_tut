57b04d2dc086b2546247c7cbb419667de4c55bab
Junior <junioragoogo3@gmail.com>
2025-02-13 11:22:56 +0000

    Created members model

--- Lession_3/Members/views.py
+++ Lession_3/Members/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
+from .models import Member
 
 # Create your views here.
 def members(request):
     template = loader.get_template('members.html')
+   
     return HttpResponse(template.render())
